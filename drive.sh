947475d36e5ffa2ab3695d5ad8e12cdcd4b78ec1
mahadi-hossain <mahadihossainpvt@gmail.com>
2021-11-11 23:38:10 +0600

    Update drive.sh

--- drive.sh
+++ drive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-fileid="1HN_eRuGeOEmYONSewqME6cBS2Rp0UxKS"
+fileid="1SVwDjFklJspqlS3mDtfuv5WlIWOq6V0F"
 filename="webgateway-1.0-SNAPSHOT.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
